3b4cf5a7860c7a04d054afb33b55cad4b02e54ff
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-06-18 17:55:41 +0200

    start hw-02-step-1_created markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,68 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import './app.css';
 
 class App extends Component {
+
   state = {
     good: 0,
     neutral: 0,
     bad: 0
   };
 
+  handleLeaveFeedback = (e) => {
+    const { name } = e.target;
+    this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+  };
+
+  
+
   render() {
     // const { good, neutral, bad } = this.state;
+    // const options = ['good', 'neutral', 'bad'];
+    // options.map(option)
+
+    // const total = this.totalFeedbackCount();
+
+
+   const buttonName = this.state.name;
 
     return (
       <>
-       <div>
-      <h1>Please leave feedback</h1>
+       <section className='Feedback'>
+        <h1 className='Feedback__title'>Please leave feedback</h1>
       
-    </div>
+      <ul className='Feedback__list'>
+        <li className='Feedback__item' key={buttonName}>
+          <button 
+          name={this.state.good}
+          className='Feedback__btn'
+          onClick={this.handleLeaveFeedback}
+          >Good</button></li>
+        <li className='Feedback__item'>
+
+          <button
+           className='Feedback__btn'
+           id='neutral'
+           onClick={()=> this.handleLeaveFeedback}
+           >Neutral</button>
+           
+           </li>
+        <li className='Feedback__item'>
+          <button 
+          className='Feedback__btn'
+          id='bad'
+          onClick={()=> this.handleLeaveFeedback}
+          >Bad</button></li>
+      </ul>
+    </section>
+    <section className='Statistics'>
+      <h1 className='Statistics__title'>Statistics</h1>
+      <ul className='Statistics__list'>
+        <li className='Statistics__item'><p className='Statistics__text'>Good: {this.state.good}</p></li>
+        <li className='Statistics__item'><p className='Statistics__text'>Neutral: {this.state.neutral}</p></li>
+        <li className='Statistics__item'><p className='Statistics__text'>Bad: {this.state.bad}</p></li>
+      </ul>
+    </section>
       </>
    
     )
@@ -23,3 +70,8 @@ class App extends Component {
 }
 
 export default App;
+
+// App.propTypes = {
+//   options: PropTypes.object.isRequired,
+//   // onLeaveFeedback: PropTypes.func.isRequired,
+// };
