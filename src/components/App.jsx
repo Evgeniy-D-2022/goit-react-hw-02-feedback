f4065bfe17be596f6169f29c57a9062566c024f2
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-06-19 13:01:19 +0200

    start hw-02-step-1_created statistics component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './app.css';
+import Statistics from './Statistics/Statistics';
 
 class App extends Component {
 
@@ -9,59 +10,57 @@ class App extends Component {
     bad: 0
   };
 
-  handleLeaveFeedback = (e) => {
-    const { name } = e.target;
+  
+
+  handleLeaveFeedback = e => {
+    const {name} = e.target;
     this.setState(prevState => ({ [name]: prevState[name] + 1 }));
   };
 
-  
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const positive = Math.round((this.state.good * 100) / this.countTotalFeedback());
+    return positive;
+  }
 
   render() {
-    // const { good, neutral, bad } = this.state;
-    // const options = ['good', 'neutral', 'bad'];
-    // options.map(option)
-
-    // const total = this.totalFeedbackCount();
-
-
-   const buttonName = this.state.name;
+    
+    const { good, neutral, bad } = this.state;
+    const names = ['good', 'neutral', 'bad'];
 
     return (
       <>
        <section className='Feedback'>
         <h1 className='Feedback__title'>Please leave feedback</h1>
+        <ul className='Feedback__list'>
       
-      <ul className='Feedback__list'>
-        <li className='Feedback__item' key={buttonName}>
-          <button 
-          name={this.state.good}
-          className='Feedback__btn'
-          onClick={this.handleLeaveFeedback}
-          >Good</button></li>
-        <li className='Feedback__item'>
-
-          <button
-           className='Feedback__btn'
-           id='neutral'
-           onClick={()=> this.handleLeaveFeedback}
-           >Neutral</button>
-           
-           </li>
-        <li className='Feedback__item'>
-          <button 
-          className='Feedback__btn'
-          id='bad'
-          onClick={()=> this.handleLeaveFeedback}
-          >Bad</button></li>
-      </ul>
+      {names.map(name => (
+      <li className='Feedback__item' key={name}>
+    <button className='Feedback__btn' 
+    name={name} type="button" 
+    onClick={this.handleLeaveFeedback}>
+      {name}
+    </button>
+  </li>
+))}
+    </ul>
+      
     </section>
     <section className='Statistics'>
       <h1 className='Statistics__title'>Statistics</h1>
-      <ul className='Statistics__list'>
-        <li className='Statistics__item'><p className='Statistics__text'>Good: {this.state.good}</p></li>
-        <li className='Statistics__item'><p className='Statistics__text'>Neutral: {this.state.neutral}</p></li>
-        <li className='Statistics__item'><p className='Statistics__text'>Bad: {this.state.bad}</p></li>
-      </ul>
+
+      < Statistics 
+       good={good}
+       neutral={neutral}
+       bad={bad}
+       total={this.countTotalFeedback()}
+       positiveFeedbackPercentage={this.countPositiveFeedbackPercentage()}/>
+      
     </section>
       </>
    
@@ -70,8 +69,3 @@ class App extends Component {
 }
 
 export default App;
-
-// App.propTypes = {
-//   options: PropTypes.object.isRequired,
-//   // onLeaveFeedback: PropTypes.func.isRequired,
-// };
